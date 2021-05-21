8c0a909797808b12a5a37a04ac89e40826bc46b8
Faris Čolaković <fcolakovic2@etf.unsa.ba>
2021-05-21 12:54:47 +0200

    zapoceto pisanje testova

---
+++ test/test.js
@@ -0,0 +1,11 @@
+let server = require(".../server")
+let chai = require("chai")
+let mocha = require("mocha")
+let chaiHttp = require ("chai-http")
+import {refresujSve} from '../tutorijal6.js'
+import {start} from '../tutorijal6.js'
+import {close} from '../tutorijal6.js'
+
+chai.should();
+chai.use(chaiHttp);
+
