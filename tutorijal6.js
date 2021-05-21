b6073f85369b026aed3e996ee8a536b7014a34cb
Faris Čolaković <fcolakovic2@etf.unsa.ba>
2021-05-21 12:34:58 +0200

    dodan start close i get zahtjev

--- tutorijal6.js
+++ tutorijal6.js
@@ -4,6 +4,56 @@ const path = require('path');
 const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 
+const porrt = 3010
+
+
+export function start(){
+  let dv = new sqlite3.Database('./db/baza.db',sqlite3.OPEN_READWRITE,(err)=>{
+    if (err){
+      return console.log(err.message)
+    }
+    console.log("Connected")
+  })
+}
+
+export function close(){
+  db.close((err)=>{
+  if (err){
+     return console.log(err.message)
+    }
+    console.log("Connected")
+  });
+}
+
+app.get('/gradovi',(req,res)=>{
+  const gradovi = [];
+
+  let db = new sqlite3.Database('./db.baza.db',sqlite3.OPEN_READWRITE,(err)=>{
+    if (err){
+      return console.log(err.message)
+    }
+    console.log("Connected");
+  })
+
+  db.serialize(()=>{
+    db.all('SELECT NAZIV FROM grad', (err,rows,fields)=>{
+      if (err){
+        return console.log(err.message)
+      }
+      res.json(JSON.stringify(rows));
+    })
+  })
+
+  db.close((err)=>{
+    if (err){
+       return console.log(err.message)
+      }
+      console.log("Connected")
+    });
+})
+
+
+
 //assuming app is express Object.
 app.get('/',function(req,res) {
     res.sendFile(__dirname+'/index.html');
